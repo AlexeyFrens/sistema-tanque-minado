efaf1aaca7760898a5adb24136169bec959b2400
João Cláudio <leiloka46@gmail.com>
2024-10-24 17:57:56 -0300

    Sistema para limpar a tela e tratamento de exceções

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,9 +1,11 @@
 package org.example.application;
 
+import org.example.game.GameException;
 import org.example.game.GameMatch;
 import org.example.game.GamePosition;
 import org.example.game.GameTankPiece;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,16 +17,24 @@ public class Main {
 
         // for now, it will stay true until the game logic is done
         while(true){
-            UI.printBoard(match.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            GamePosition source = UI.readGamePosition(scanner);
 
-            System.out.println();
-            System.out.print("Target: ");
-            GamePosition target = UI.readGamePosition(scanner);
+            try {
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                GamePosition source = UI.readGamePosition(scanner);
 
-            GameTankPiece capturedPiece = match.performGameMove(source, target);
+                System.out.println();
+                System.out.print("Target: ");
+                GamePosition target = UI.readGamePosition(scanner);
+
+                GameTankPiece capturedPiece = match.performGameMove(source, target);
+            }catch (GameException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                System.out.println("\npress enter to continue");
+                scanner.nextLine();
+            }
         }
     }
 }
