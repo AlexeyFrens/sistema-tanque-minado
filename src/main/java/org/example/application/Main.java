20b5db6833fcdd1fbdeea40010d3a63d66a412c0
João Cláudio <leiloka46@gmail.com>
2024-10-24 16:42:43 -0300

    Movendo paças sem limitações

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,10 +1,30 @@
 package org.example.application;
 
 import org.example.game.GameMatch;
+import org.example.game.GamePosition;
+import org.example.game.GameTankPiece;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         GameMatch match = new GameMatch();
-        UI.printBoard(match.getPieces());
+
+        // for now, it will stay true until the game logic is done
+        while(true){
+            UI.printBoard(match.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            GamePosition source = UI.readGamePosition(scanner);
+
+            System.out.println();
+            System.out.print("Target: ");
+            GamePosition target = UI.readGamePosition(scanner);
+
+            GameTankPiece capturedPiece = match.performGameMove(source, target);
+        }
     }
 }
--- src/main/java/org/example/game/GameMatch.java
+++ src/main/java/org/example/game/GameMatch.java
@@ -1,6 +1,8 @@
 package org.example.game;
 
 import org.example.boardGame.Board;
+import org.example.boardGame.Piece;
+import org.example.boardGame.Position;
 
 public class GameMatch {
     private Board board;
@@ -18,6 +20,29 @@ public class GameMatch {
             }
         }
         return mat;
+    }
+
+    public GameTankPiece performGameMove(GamePosition sourcePosition, GamePosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+
+        return (GameTankPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+
+        return capturedPiece;
+    }
+
+    public void validateSourcePosition(Position position) {
+        if(!board.thereIsAPiece(position)) {
+            throw new GameException("There is no piece on source position");
+        }
     }
 
     private void placeNewPiece(char column, int row, GameTankPiece piece) {
